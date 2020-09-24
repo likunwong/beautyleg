fd28abd06dc0439056c36c745f81e888da675706
likunwong <71484247+likunwong@users.noreply.github.com>
2020-09-24 17:57:05 +0800

    Create tree.cpp

---
+++ tree.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    Node *left;
+    Node *right;
+    int value;
+}
+
+void insertToTree(Node *root, int value)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (root -> left != NULL && value < root -> value) 
+    {
+        return insertToTree(root -> left, value);
+    }
+    if (root -> right != NULL && value > root -> value)
+    {
+        return insertToTree(root -> right, value);
+    }
+    if (value < root -> value && root -> left == NULL)
+    {
+        root -> left = new Node{value: value};
+        return 
+    }
+    if (value > root -> value && root -> right == NULL)
+    {
+        root -> right = new Node{value: value};
+        return 
+    }
+}
